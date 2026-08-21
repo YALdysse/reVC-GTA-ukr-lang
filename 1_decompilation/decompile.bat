82b28d6b8ba878a9b38e661b4aa5157a0aca1346
YALdysse <yaroslav_lytvynov@yahoo.com>
2026-08-22 00:11:01 +0300

    Initial commit

---
+++ 1_decompilation/decompile.bat
@@ -0,0 +1,2 @@
+..\gxt.exe -i ".\source\ukrainian_pc_andrulko.gxt" -o ".\destination\ukrainian_pc_andrulko.txt" -t ".\tables\vc_table_pc_ukr_andrulko.txt"
+pause
---
+++ 2_compilation/compile_for_reVC.bat
@@ -0,0 +1,2 @@
+..\gxt.exe -i ".\source\ukrainian_pc_andrulko_utf-8-bom.txt" -o ".\destination\ukrainian.gxt" -t ".\tables\vc_table_pc_ukr_andrulko.txt"
+pause
